bf7bd079292fda37f4c30edac6bd2c8f7775da03
Ayan-2014yo <67223318+Ayan-2014yo@users.noreply.github.com>
2022-05-12 17:59:58 +0600

    Add files via upload

---
+++ endterm3-main/CRUD — копия/src/main/java/com/example/CRUD/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.example.CRUD.controller;
+
+import com.example.CRUD.model.User;
+import com.example.CRUD.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+
+import java.util.List;
+
+@Controller
+@Api(value = "User Controller class", description = "This class allows to interact with User object")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/users")
+    @ApiOperation(value = "Method to get list of users", response = List.class)
+    public String findAll(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "user-list";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user) {
+        return "user-create";
+
+    }
+
+    @PostMapping("/user-create")
+    public String createUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("user-delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
+
+    @GetMapping("user-update/{id}")
+    public String updateUserForm(@PathVariable("id") Long id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "/user-update";
+    }
+
+
+    @PostMapping("/user-update")
+    public String updateUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+}
---
+++ endterm3-main/CRUD — копия/src/main/java/com/example/CRUD/model/Role.java
@@ -0,0 +1,6 @@
+package com.example.CRUD.model;
+
+public enum Role {
+    USER,
+    ADMIN;
+}
